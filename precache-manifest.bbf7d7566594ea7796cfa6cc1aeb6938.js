877135f62046732cd16c836d75b9d49e4d74f719
Shivam Dubey <dshivam227@gmail.com>
2022-07-24 23:46:29 +0530

    Updates

---
+++ precache-manifest.bbf7d7566594ea7796cfa6cc1aeb6938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fd6392479966ecb7c76",
+    "url": "/textutils-react/static/css/main.b9b490c5.chunk.css"
+  },
+  {
+    "revision": "6fd6392479966ecb7c76",
+    "url": "/textutils-react/static/js/main.6fd63924.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils-react/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils-react/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "3f07bacf06d32ccfbe48",
+    "url": "/textutils-react/static/js/runtime~main.3f07bacf.js"
+  },
+  {
+    "revision": "2ff1ce46dac901960c37df80ec21efbe",
+    "url": "/textutils-react/index.html"
+  }
+];
